c06bb8a20431fc9b98bc1c0ac9126ad7f6c50a37
slowspirit <slowspirit@gmail.com>
2022-02-03 14:49:19 +0300

    added UI (UIkit 3) & improved buttons functionality

--- main.js
+++ main.js
@@ -4,28 +4,44 @@ document.querySelector('.selectBtn').addEventListener('click', exploreSelection)
 document.querySelector('.hideBtn').addEventListener('click', hideChord);
 document.querySelector('.copyBtn').addEventListener('click', copying);
 
+
+function watchingShowplace() {
+    let watchedElem = document.querySelector('.showedChord').textContent;
+    if (watchedElem === 'Please, choose the chord') {
+        document.querySelector('.hideBtn').setAttribute("disabled", "disabled");
+    } else {
+        document.querySelector('.hideBtn').removeAttribute("disabled", "disabled");
+    }
+}
+
 function exploreSelection() {
+    document.querySelector('.hideBtn').removeAttribute("disabled", "disabled");
+    // document.querySelector('.copyBtn').removeAttribute("disabled", "disabled");
     let selectedMeaning1 = document.getElementById('list_of_chords1');
     let selectedMeaning2 = document.getElementById('list_of_chords2');
     let wholeChordName = 'chord_' + selectedMeaning1.value + selectedMeaning2.value;
-    // console.log(wholeChordName);
     let area = document.getElementById('showedChord');
+
 
     if (wholeChordName !== 'chord_' && (selectedMeaning1.value !== '' && selectedMeaning2 !== '')) {
         area.innerHTML = chordsArray[wholeChordName];
         area.style.display = 'inline';
+        document.querySelector('.copyBtn').removeAttribute("disabled", "disabled");
     } else if (wholeChordName == 'chord_' && (selectedMeaning1.value == '' && selectedMeaning2.value == '')) {
         area.innerHTML = 'Please, choose the chord';
+        document.querySelector('.hideBtn').setAttribute("disabled", "disabled");
     } else {
         area.innerHTML = 'Oops!<br>Such chord dose not exist';
+        document.querySelector('.copyBtn').setAttribute("disabled", "disabled");
     }
     return chordsArray[wholeChordName];
 }
 
 function hideChord() {
     let hiddenElement = document.getElementById('showedChord');
-    // console.log(hiddenElement);
-    hiddenElement.innerHTML = 'Please, choose the chord';
+    hiddenElement.innerText = 'Please, choose the chord';
+    document.querySelector('.hideBtn').setAttribute("disabled", "disabled");
+    document.querySelector('.copyBtn').setAttribute("disabled", "disabled");
 }
 
 function copying() {
@@ -34,6 +50,5 @@ function copying() {
         let copiedElement = document.createElement('span');
         copiedElement.innerHTML = exploreSelection();
         safezone.appendChild(copiedElement);
-        // console.log(exploreSelection());
     }
 }
